49a38c4742ecefafdf1a12c87fba0b7ea052c925
Yusuf Onur Zengin <yusufonurzengin@gmail.com>
2019-06-15 16:35:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 setlocale(LC_ALL, 'tr_TR.UTF-8');
 session_start();
+echo "naber";
 define("ANA_DIZIN", "asds");
 define("SERVER", "https://www.kurasepeti.com/");
 define("DOSYAYOL",$_SERVER['DOCUMENT_ROOT']."/kurasepeti/");
 // $_SERVER['DOCUMENT_ROOT']."/"
 define("JSDEGIS",time());
-require_once 'uygulama/init.php';
 $uygulama = new Uygulamalar;
