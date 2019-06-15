328d49e9b1ccc4d63e1d1fe2ddf96212160794c2
Yusuf Onur Zengin <yusufonurzengin@gmail.com>
2019-06-15 16:46:38 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
-=======
-<?php
-setlocale(LC_ALL, 'tr_TR.UTF-8');
-session_start();
-define("ANA_DIZIN", "http://localhost/kurasepeti/");
-define("SERVER", "https://www.kurasepeti.com/");
-define("DOSYAYOL",$_SERVER['DOCUMENT_ROOT']."/kurasepeti/");
-// $_SERVER['DOCUMENT_ROOT']."/"
-define("JSDEGIS",time());
-require_once 'uygulama/init.php';
-$uygulama = new Uygulamalar;
->>>>>>> parent of d6956e5... Update index.php
-asdasd
+1
+2
+3
+11
+5
+6
+7
+8
